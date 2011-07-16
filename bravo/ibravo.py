2c56e2a9d3247932079772221b5a9ad40c08e703
Mike Cooper <mythmon@gmail.com>
2011-07-16 13:14:31 -0400

    Add `bravo.plugin` and dependencies.
    https://github.com/MostAwesomeDude/bravo/blob/master/bravo/plugin.py

---
+++ bravo/ibravo.py
@@ -0,0 +1,456 @@
+from twisted.python.components import registerAdapter
+from twisted.web.resource import IResource
+from zope.interface import implements, invariant, Attribute, Interface
+
+from bravo.errors import InvariantException
+
+class IBravoPlugin(Interface):
+    """
+    Interface for plugins.
+
+    This interface stores common metadata used during plugin discovery.
+    """
+
+    name = Attribute("""
+        The name of the plugin.
+
+        This name is used to reference the plugin in configurations, and also
+        to uniquely index the plugin.
+        """)
+
+def sorted_invariant(s):
+    intersection = set(s.before) & set(s.after)
+    if intersection:
+        raise InvariantException("Plugin wants to come before and after %r" %
+            intersection)
+
+class ISortedPlugin(IBravoPlugin):
+    """
+    Parent interface for sorted plugins.
+
+    Sorted plugins have an innate and automatic ordering inside lists thanks
+    to the ability to advertise their dependencies.
+    """
+
+    invariant(sorted_invariant)
+
+    before = Attribute("""
+        Plugins which must come before this plugin in the pipeline.
+
+        Should be a tuple, list, or some other iterable.
+        """)
+
+    after = Attribute("""
+        Plugins which must come after this plugin in the pipeline.
+
+        Should be a tuple, list, or some other iterable.
+        """)
+
+class IAuthenticator(IBravoPlugin):
+    """
+    Interface for authenticators.
+
+    Authenticators participate in a two-step login process with a handshake.
+    """
+
+    def handshake(protocol, container):
+        """
+        Handle a handshake.
+
+        This function should always return True or False, depending on whether
+        the handshake was successful.
+        """
+
+    def login(protocol, container):
+        """
+        Handle a login.
+
+        This function should return a ``Deferred`` which will fire depending
+        on whether the login was successful.
+        """
+
+class ITerrainGenerator(ISortedPlugin):
+    """
+    Interface for terrain generators.
+    """
+
+    def populate(chunk, seed):
+        """
+        Given a chunk and a seed value, populate the chunk with terrain.
+
+        This function should assume that it runs as part of a pipeline, and
+        that the chunk may already be partially or totally populated.
+        """
+
+def command_invariant(c):
+    if c.__doc__ is None:
+        raise InvariantException("Command has no documentation")
+
+class ICommand(IBravoPlugin):
+    """
+    A command.
+
+    Commands must be documented, as an invariant. The documentation for a
+    command will be displayed for clients upon request, via internal help
+    commands.
+    """
+
+    invariant(command_invariant)
+
+    aliases = Attribute("""
+        Additional keywords which may be used to alias this command.
+        """)
+
+    usage = Attribute("""
+        String explaining how to use this command.
+        """)
+
+class IChatCommand(ICommand):
+    """
+    Interface for chat commands.
+
+    Chat commands are invoked from the chat inside clients, so they are always
+    called by a specific client.
+
+    This interface is specifically designed to exist comfortably side-by-side
+    with `IConsoleCommand`.
+    """
+
+    def chat_command(username, parameters):
+        """
+        Handle a command from the chat interface.
+
+        :param str username: username of player
+        :param list parameters: additional parameters passed to the command
+
+        :returns: a generator object or other iterable yielding lines
+        """
+
+class IConsoleCommand(ICommand):
+    """
+    Interface for console commands.
+
+    Console commands are invoked from a console or some other location with
+    two defining attributes: Access restricted to superusers, and no user
+    issuing the command. As such, no access control list applies to them, but
+    they must be given usernames to operate on explicitly.
+    """
+
+    def console_command(parameters):
+        """
+        Handle a command.
+
+        :param list parameters: additional parameters passed to the command
+
+        :returns: a generator object or other iterable yielding lines
+        """
+
+class ChatToConsole(object):
+    """
+    Adapt a chat command to be used on the console.
+
+    This largely consists of passing the username correctly.
+    """
+
+    implements(IConsoleCommand)
+
+    def __init__(self, chatcommand):
+        self.chatcommand = chatcommand
+
+        self.aliases = self.chatcommand.aliases
+        self.info = self.chatcommand.info
+        self.name = self.chatcommand.name
+        self.usage = "<username> %s" % self.chatcommand.usage
+
+    def console_command(self, parameters):
+        if IConsoleCommand.providedBy(self.chatcommand):
+            return self.chatcommand.console_command(parameters)
+        else:
+            username = parameters.pop(0) if parameters else ""
+            return self.chatcommand.chat_command(username, parameters)
+
+registerAdapter(ChatToConsole, IChatCommand, IConsoleCommand)
+
+def recipe_invariant(r):
+    # Size invariant.
+    if len(r.recipe) != r.dimensions[0] * r.dimensions[1]:
+        raise InvariantException("Recipe size is invalid")
+
+class IRecipe(IBravoPlugin):
+    """
+    Recipe for crafting materials from other materials.
+    """
+
+    invariant(recipe_invariant)
+
+    dimensions = Attribute("""
+        Tuple representing the size of the recipe.
+        """)
+
+    recipe = Attribute("""
+        Tuple representing the items of the recipe.
+
+        Recipes need to be filled out left-to-right, top-to-bottom, with one
+        of two things:
+
+         * A tuple (slot, count) for the item/block that needs to be present;
+         * None, if the slot needs to be empty.
+        """)
+
+    provides = Attribute("""
+        Tuple representing the yield of this recipe.
+
+        This tuple must be of the format (slot, count).
+        """)
+
+class ISeason(IBravoPlugin):
+    """
+    Seasons are transformational stages run during certain days to emulate an
+    environment.
+    """
+
+    def transform(chunk):
+        """
+        Apply the season to the given chunk.
+        """
+
+    day = Attribute("""
+        Day of the year on which to switch to this season.
+        """)
+
+class ISerializer(IBravoPlugin):
+    """
+    Class that understands how to serialize several different kinds of objects
+    to and from disk-friendly formats.
+
+    Implementors of this interface are expected to provide a uniform
+    implementation of their serialization technique.
+    """
+
+    def save_chunk(chunk):
+        """
+        Save a chunk.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def load_chunk(chunk):
+        """
+        Load a chunk.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def save_level(level):
+        """
+        Save a level.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def load_level(level):
+        """
+        Load a level.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def save_player(player):
+        """
+        Save a player.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def load_player(player):
+        """
+        Load a player.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def load_plugin_data(name):
+        """
+        Load plugin-specific data.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+    def save_plugin_data(name, value):
+        """
+        Save plugin-specific data.
+
+        May return a ``Deferred`` that will fire on completion.
+        """
+
+class ISerializerFactory(IBravoPlugin):
+    """
+    Factory for ``ISerializer`` instances.
+
+    I am so sorry for this.
+    """
+
+# Hooks
+
+class IPreBuildHook(ISortedPlugin):
+    """
+    Hook for actions to be taken before a block is placed.
+    """
+
+    def pre_build_hook(player, builddata):
+        """
+        Do things.
+
+        The ``player`` is a ``Player`` entity and can be modified as needed.
+
+        The ``builddata`` tuple has all of the useful things. It stores a
+        ``Block`` that will be placed, as well as the block coordinates and
+        face of the place where the block will be built.
+
+        ``builddata`` needs to be passed to the next hook in sequence, but it
+        can be modified in passing in order to modify the way blocks are
+        placed.
+
+        Any access to chunks must be done through the factory. To get the
+        current factory, import it from ``bravo.parameters``:
+
+        >>> from bravo.parameters import factory
+
+        The second variable in the return value indicates whether processing
+        of building should continue after this hook runs. Use it to halt build
+        hook processing, if needed.
+
+        For sanity purposes, build hooks may return a ``Deferred`` which will
+        fire with their return values, but are not obligated to do so.
+
+        A trivial do-nothing build hook looks like the following:
+
+        >>> def pre_build_hook(self, player, builddata):
+        ...     return True, builddata
+
+        To make life more pleasant when returning deferred values, use
+        ``inlineCallbacks``, which many of the standard build hooks use:
+
+        >>> @inlineCallbacks
+        ... def pre_build_hook(self, player, builddata):
+        ...     returnValue((True, builddata))
+
+        This form makes it much easier to deal with asynchronous operations on
+        the factory and world.
+
+        :param ``Player`` player: player entity doing the building
+        :param namedtuple builddata: permanent building location and data
+
+        :returns: ``Deferred`` with tuple of build data and whether subsequent
+                  hooks will run
+        """
+
+class IPostBuildHook(ISortedPlugin):
+    """
+    Hook for actions to be taken after a block is placed.
+    """
+
+    def post_build_hook(player, coords, block):
+        """
+        Do things.
+        """
+
+class IDigHook(ISortedPlugin):
+    """
+    Hook for actions to be taken after a block is dug up.
+    """
+
+    def dig_hook(chunk, x, y, z, block):
+        """
+        Do things.
+
+        :param `Chunk` chunk: digging location
+        :param int x: X coordinate
+        :param int y: Y coordinate
+        :param int z: Z coordinate
+        :param `Block` block: dug block
+        """
+
+class ISignHook(ISortedPlugin):
+    """
+    Hook for actions to be taken after a sign is updated.
+
+    This hook fires both on sign creation and sign editing.
+    """
+
+    def sign_hook(chunk, x, y, z, text, new):
+        """
+        Do things.
+
+        :param `Chunk` chunk: digging location
+        :param int x: X coordinate
+        :param int y: Y coordinate
+        :param int z: Z coordinate
+        :param list text: list of lines of text
+        :param bool new: whether this sign is newly placed
+        """
+
+class IUseHook(ISortedPlugin):
+    """
+    Hook for actions to be taken when a player interacts with an entity.
+
+    Each plugin needs to specify a list of entity types it is interested in
+    in advance, and it will only be called for those.
+    """
+
+    def use_hook(player, target, alternate):
+        """
+        Do things.
+
+        :param `Player` player: player
+        :param `Entity` target: target of the interaction
+        :param bool alternate: whether the player right-clicked the target
+        """
+
+    targets = Attribute("""
+        List of entity names this plugin wants to be called for.
+        """)
+
+class IAutomaton(IBravoPlugin):
+    """
+    An automaton.
+
+    Automatons are given blocks from chunks which interest them, and may do
+    processing on those blocks.
+    """
+
+    blocks = Attribute("""
+        List of blocks which this automaton is interested in.
+        """)
+
+    def feed(coordinates):
+        """
+        Provide this automaton with block coordinates to handle later.
+        """
+
+    def scan(chunk):
+        """
+        Provide this automaton with an entire chunk which this automaton may
+        handle as it pleases.
+
+        A utility scanner which will simply `feed()` this automaton is in
+        bravo.utilities.automatic.
+        """
+
+    def start():
+        """
+        Run the automaton.
+        """
+
+    def stop():
+        """
+        Stop the automaton.
+
+        After this method is called, the automaton should not continue
+        processing data; it needs to stop immediately.
+        """
+
+class IWorldResource(IBravoPlugin, IResource):
+    """
+    Interface for a world specific web resource.
+    """
---
+++ bravo/plugin.py
@@ -0,0 +1,292 @@
+"""
+The ``plugin`` module implements a sophisticated, featureful plugin loader
+based on Exocet, with interface-based discovery.
+"""
+
+from types import ModuleType
+from xml.sax import saxutils
+
+from exocet import ExclusiveMapper, getModule, load, pep302Mapper
+
+from twisted.internet import reactor
+from twisted.python import log
+
+from zope.interface.exceptions import BrokenImplementation
+from zope.interface.exceptions import BrokenMethodImplementation
+from zope.interface.verify import verifyObject
+
+from bravo.errors import PluginException
+from bravo.ibravo import InvariantException, ISortedPlugin
+
+blacklisted = set([
+    "asyncore",        # Use Twisted's event loop.
+    "ctypes",          # Segfault protection.
+    "gc",              # Haha, no.
+    "imp",             # Haha, no.
+    "inspect",         # Haha, no.
+    "multiprocessing", # Use Twisted's process interface.
+    "socket",          # Use Twisted's socket interface.
+    "subprocess",      # Use Twisted's process interface.
+    "thread",          # Use Twisted's thread interface.
+    "threading",       # Use Twisted's thread interface.
+])
+overrides = {
+    "twisted.internet.reactor": reactor,
+    "saxutils": saxutils,
+}
+bravoMapper = ExclusiveMapper(pep302Mapper,
+                              blacklisted).withOverrides(overrides)
+
+def sort_plugins(plugins):
+    """
+    Make a sorted list of plugins by dependency.
+
+    If the list cannot be arranged into a DAG, an error will be raised. This
+    usually means that a cyclic dependency was found.
+
+    :raises PluginException: cyclic dependency detected
+    """
+
+    l = []
+    d = dict((plugin.name, plugin) for plugin in plugins)
+
+    def visit(plugin):
+        if plugin not in l:
+            for name in plugin.before:
+                if name in d:
+                    visit(d[name])
+            l.append(plugin)
+
+    for plugin in plugins:
+        if not any(name in d for name in plugin.after):
+            visit(plugin)
+
+    return l
+
+def add_plugin_edges(d):
+    """
+    Mirror edges to all plugins in a dictionary.
+    """
+
+    for plugin in d.itervalues():
+        plugin.after = set(plugin.after)
+        plugin.before = set(plugin.before)
+
+    for name, plugin in d.iteritems():
+        for edge in list(plugin.before):
+            if edge in d:
+                d[edge].after.add(name)
+            else:
+                plugin.before.discard(edge)
+        for edge in list(plugin.after):
+            if edge in d:
+                d[edge].before.add(name)
+            else:
+                plugin.after.discard(edge)
+
+    return d
+
+def expand_names(plugins, names):
+    """
+    Given a list of names, expand wildcards and discard disabled names.
+
+    Used to implement * and - options in plugin lists.
+
+    :param dict plugins: plugins to use for expansion
+    :param list names: names to examine
+
+    :returns: a list of filtered plugin names
+    """
+
+    # Wildcard.
+    if "*" in names:
+        # Get the exceptions.
+        exceptions = set(name[1:] for name in names if name.startswith("-"))
+
+        # And now the names. Everything that isn't excepted.
+        names = [name for name in plugins.iterkeys()
+            if name not in exceptions]
+
+    return names
+
+def verify_plugin(interface, plugin):
+    """
+    Plugin interface verification.
+
+    This function will call ``verifyObject()`` and ``validateInvariants()`` on
+    the plugins passed to it.
+
+    The primary purpose of this wrapper is to do logging, but it also permits
+    code to be slightly cleaner, easier to test, and callable from other
+    modules.
+    """
+
+    try:
+        verifyObject(interface, plugin)
+        interface.validateInvariants(plugin)
+        log.msg(" ( ^^) Plugin: %s" % plugin.name)
+    except BrokenImplementation, bi:
+        if hasattr(plugin, "name"):
+            log.msg(" ( ~~) Plugin %s is missing attribute %r!" %
+                (plugin.name, bi.name))
+        else:
+            log.msg(" ( >&) Plugin %s is unnamed and useless!" % plugin)
+    except BrokenMethodImplementation, bmi:
+        log.msg(" ( Oo) Plugin %s has a broken %s()!" % (plugin.name,
+            bmi.method))
+        log.msg(bmi)
+    except InvariantException, ie:
+        log.msg(" ( >&) Plugin %s failed validation!" % plugin.name)
+        log.msg(ie)
+    else:
+        return plugin
+
+    raise PluginException("Plugin failed verification")
+
+def synthesize_parameters(parameters):
+    """
+    Create a faked module which has the given parameters in it.
+
+    This should work everywhere. If it doesn't, let me know.
+    """
+
+    module = ModuleType("parameters")
+    module.__dict__.update(parameters)
+    return module
+
+__cache = {}
+
+def get_plugins(interface, package, parameters=None):
+    """
+    Lazily find objects in a package which implement a given interface.
+
+    If the optional dictionary of parameters is provided, it will be passed
+    into each plugin module as the "bravo.parameters" module. An example
+    access from inside the plugin:
+
+    >>> from bravo.parameters import foo, bar
+
+    Since the parameters are available as a real module, the parameters may be
+    imported and used like any other module:
+
+    >>> from bravo import parameters as params
+
+    This is a rewrite of Twisted's ``twisted.plugin.getPlugins`` which uses
+    Exocet instead of Twisted to find the plugins.
+
+    :param interface interface: the interface to match against
+    :param str package: the name of the package to search
+    :param dict parameters: parameters to pass into the plugins
+    """
+
+    mapper = bravoMapper
+
+    # If parameters are provided, add them to the mapper in a synthetic
+    # module.
+    if parameters:
+        mapper = mapper.withOverrides(
+            {"bravo.parameters": synthesize_parameters(parameters)})
+
+    # This stack will let us iteratively recurse into packages during the
+    # module search.
+    stack = [getModule(package)]
+
+    # While there are packages left to search...
+    while stack:
+        # For each package/module in the package...
+        for pm in stack.pop().iterModules():
+            # If it's a package, append it to the list of packages to search.
+            if pm.isPackage():
+                stack.append(pm)
+
+            try:
+                # Load the module.
+                m = load(pm, mapper)
+
+                # Make a good attempt to iterate through the module's
+                # contents, and see what matches our interface.
+                for obj in vars(m).itervalues():
+                    try:
+                        adapted = interface(obj, None)
+                    except:
+                        log.err()
+                    else:
+                        if adapted is not None:
+                            yield adapted
+            except ImportError, ie:
+                log.msg(ie)
+
+def retrieve_plugins(interface, parameters=None):
+    """
+    Look up all plugins for a certain interface.
+
+    If the plugin cache is enabled, this function will not attempt to reload
+    plugins from disk or discover new plugins.
+
+    :param interface interface: the interface to use
+    :param dict parameters: parameters to pass into the plugins
+
+    :returns: a dict of plugins, keyed by name
+    :raises PluginException: no plugins could be found for the given interface
+    """
+
+    if not parameters and interface in __cache:
+        return __cache[interface]
+
+    log.msg("Discovering %s..." % interface)
+    d = {}
+    for p in get_plugins(interface, "bravo.plugins", parameters):
+        try:
+            verify_plugin(interface, p)
+            d[p.name] = p
+        except PluginException:
+            pass
+
+    if issubclass(interface, ISortedPlugin):
+        # Sortable plugins need their edges mirrored.
+        d = add_plugin_edges(d)
+
+    # Cache non-parameterized plugins.
+    if not parameters:
+        __cache[interface] = d
+
+    return d
+
+def retrieve_named_plugins(interface, names, parameters=None):
+    """
+    Look up a list of plugins by name.
+
+    Plugins are returned in the same order as their names.
+
+    :param interface interface: the interface to use
+    :param list names: plugins to find
+    :param dict parameters: parameters to pass into the plugins
+
+    :returns: a list of plugins
+    :raises PluginException: no plugins could be found for the given interface
+    """
+
+    d = retrieve_plugins(interface, parameters)
+
+    # Handle wildcards and options.
+    names = expand_names(d, names)
+
+    try:
+        return [d[name] for name in names]
+    except KeyError, e:
+        raise PluginException("Couldn't find plugin %s for interface %s!" %
+            (e.args[0], interface.__name__))
+
+def retrieve_sorted_plugins(interface, names, parameters=None):
+    """
+    Look up a list of plugins, sorted by interdependencies.
+
+    :param dict parameters: parameters to pass into the plugins
+    """
+
+    l = retrieve_named_plugins(interface, names, parameters)
+    try:
+        return sort_plugins(l)
+    except KeyError, e:
+        raise PluginException("Couldn't find plugin %s for interface %s!" %
+            (e.args[0], interface))
