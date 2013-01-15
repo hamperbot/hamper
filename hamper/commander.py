9a782d51aefef04425ce1f5c687551121fed97ed
Chance Zibolsk <chance.zibolski@gmail.com>
2013-01-14 21:42:55 -0800

    Reconnect if connection lost

--- hamper/commander.py
+++ hamper/commander.py
@@ -224,6 +224,8 @@ class CommanderFactory(protocol.ClientFactory):
 
     def clientConnectionLost(self, connector, reason):
         print "Lost connection (%s)." % (reason)
+        # Reconnect
+        connector.connect()
 
     def clientConnectionFailed(self, connector, reason):
         print "Could not connect: %s" % (reason,)
