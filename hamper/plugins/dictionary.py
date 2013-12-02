37dded4811c3db276d982c2a1d1b469314133a6c
uberj <uberj@onid.orst.edu>
2013-12-01 18:35:56 -0800

    dictionary plugin

---
+++ hamper/plugins/dictionary.py
@@ -0,0 +1,96 @@
+from hamper.interfaces import ChatCommandPlugin, Command
+
+try:
+    # Python 2
+    import HTMLParser
+    html = HTMLParser.HTMLParser()
+except ImportError:
+    # Python 3
+    import html.parser
+    html = html.parser.HTMLParser()
+
+
+import re
+import requests
+import json
+
+
+class Lookup(ChatCommandPlugin):
+    name = 'lookup'
+    priority = 2
+
+    def setup(self, loader):
+        self.search_url = "http://www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q={query}&sl=en&tl=en&restrict=pr%2Cde&client=te"  # noqa
+        super(Lookup, self).setup(loader)
+
+    class Lookup(Command):
+        ''' '''
+        name = 'lookup'
+        regex = '^(lookup\s+and\s+cite|lookup)\s*(\d+)?\s+(.*)'
+
+        long_desc = short_desc = (
+            "Look up word root on http://en.wikipedia.org/wiki/List_of_medical_roots,_suffixes_and_prefixes"  # noqa
+        )
+
+        def command(self, bot, comm, groups):
+            lookup_type = groups[0]
+            def_num = int(groups[1]) if groups[1] else 1
+            query = groups[2]
+
+            resp = requests.get(self.plugin.search_url.format(query=query))
+            if resp.status_code != 200:
+                raise Exception(
+                    "Lookup Error: A non 200 status code was returned"
+                )
+
+            # get rid of google cruft
+            gr = resp.content.strip('dict_api.callbacks.id100(')
+            gr = gr.strip(',200,null)')
+
+            gr = gr.replace('\\x', "\u00")  # Google uses javascript JSON crap
+            gr = json.loads(gr)
+
+            if 'primaries' in gr:
+                entries = gr['primaries'][0]['entries']
+            elif 'webDefinitions' in gr:
+                entries = gr['webDefinitions'][0]['entries']
+            else:
+                bot.reply(comm, "No definition found")
+                return False
+
+            seen = 0
+            definition = None
+            url = None
+            for entry in entries:
+                if not entry['type'] == 'meaning':
+                    continue
+
+                seen += 1
+                if seen != def_num:
+                    continue
+
+                for term in entry['terms']:
+                    if term['type'] == 'url':
+                        url = re.sub('<[^<]+?>', '', term['text'])
+                    else:
+                        definition = term['text']
+                break
+
+            if not definition:
+                bot.reply(
+                    comm,
+                    "Looks like there might not be %s definitions" % def_num
+                )
+            else:
+                bot.reply(comm, html.unescape(definition))
+                if 'cite' in lookup_type:
+                    if url:
+                        bot.reply(comm, html.unescape(url))
+                    else:
+                        bot.reply(comm, '[No citation]')
+
+            # Always let the other plugins run
+            return False
+
+
+lookup = Lookup()
