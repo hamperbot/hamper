78d863628448a9da45c5da8b0bcdced2f6e6e3e3
Mike Cooper <mythmon@gmail.com>
2016-03-10 15:13:22 -0800

    v1.8.2 bump more dependency versions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = open('requirements.txt').read().split('\n')
 
 setup(
     name='hamper',
-    version='1.8.1',
+    version='1.8.2',
     description='Yet another IRC bot',
     install_requires=requires,
     author='Mike Cooper',
